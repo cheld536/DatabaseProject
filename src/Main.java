2031501005c980c470a1da2a30f393ebdba37be5
Yoonhee kim <kss7035@naver.com>
2022-12-04 15:18:53 +0900

    직원 정보 삽입 함수 구현

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,9 @@ public class Main
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://192.168.36.3:4567/madang", "yunhee","1234");       // mysql madang databases 연결
+                    "jdbc:mysql://192.168.36.3:4567/STORE", "yunhee","1234");       // mysql madang databases 연결
             Statement stmt=con.createStatement();                                                     // 쿼리문 생성을 위한 객체
-            ResultSet rs=stmt.executeQuery("SELECT * FROM Book");                                 // 쿼리문을 담는 객체
 
-            System.out.println("\n\n\n 안녕하세요. 현재 Madang DB Book Table 현황입니다. \n\n\n ");
-            while(rs.next())
-                System.out.println(rs.getInt(1)+" "+rs.getString(2)+ " "+rs.getString(3) + " " +rs.getInt(4));
-            con.close();
 
             /* Mading database Book table*/
             Scanner scan = new Scanner(System.in);      // 검색을 위한 객체 생성
@@ -50,7 +45,7 @@ public class Main
                     Search_data();
                 }else if (menunumber_int==4){       // 전체 데이터 확인
                     Select_All();
-                } else if (menunumber_int ==5 ) {   // 종료
+                } else if (menunumber_int ==0 ) {   // 종료
                     System.out.println("\n 프로그램을 종료합니다 \n");
                     break;
                 }
@@ -165,11 +160,78 @@ public class Main
     }
 
     /*연결 */
-    public static void connection(){}
+    public static void connection(){
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://192.168.36.3:4567/madang", "yunhee","1234");       // mysql madang databases 연결
+            Statement stmt=con.createStatement();                                                     // 쿼리문 생성을 위한 객체
+
+            /* Mading database Book table*/
+            Scanner scan = new Scanner(System.in);      // 검색을 위한 객체 생성
+            System.out.println("---------------------------------------------------\n");
+            System.out.println("\n\t연결되었습니다.\n");
+            System.out.println("---------------------------------------------------\n");
+
+        }catch(Exception e){ System.out.println(e);}
+    }
 
     /*직원 정보*/
-    public static void Search_data_all_employee(){}
-    public static void New_employee(){}
+    public static void Search_data_all_employee(){
+
+    }
+    public static void New_employee(){
+        /*table 데이터*/
+        String empolyee_id; //자동생성으로 필요없음
+        String empolyee_name;
+        String Gender;
+        String age;
+        String Phonenumber;
+        String Salary;
+        String worksdate;
+        String branchcode;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.36.3:4567/STORE", "yunhee", "1234");  // 연결을 위한 con 변수
+            String sql = "insert into empolyee values(?,?,?,?,?,?,?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("추가 할 직원의 이름을 입력하세요.(필수) : ");
+            empolyee_name = scan.nextLine();
+            System.out.println("추가 할  직원의 성별을 입력하세요.(필수) : ");
+            Gender = scan.nextLine();
+            System.out.println("추가 할 직원의 나이를 입력하세요. : ");
+            age = scan.nextLine();
+            System.out.println("추가 할 직원의 전화번호를 입력하세요(010-1234-5678)(필수). : ");
+            Phonenumber = scan.nextLine();
+            System.out.println("추가 할 직원의 월급을 입력하세요. : ");
+            Salary = scan.nextLine();
+            System.out.println("추가 할 직원이 일하는 날짜를 입력하세요.(YYYY-MM-DD) : ");
+            worksdate = scan.nextLine();
+            System.out.println("추가 할 직원의 일하는 곳을 입력하세요. : ");
+            branchcode = scan.nextLine();
+
+
+            stmt.setString(1, empolyee_name);
+            stmt.setString(2, Gender);
+            stmt.setString(3, age);
+            stmt.setString(4, Phonenumber);
+            stmt.setInt(5, Integer.parseInt(Salary));
+            stmt.setString(6,worksdate);
+            stmt.setInt(7, Integer.parseInt(branchcode));
+
+
+            int result = stmt.executeUpdate();
+            if(result ==1) System.out.println("Inset 저장 성공!");
+            else System.out.println("저장 실패");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
     public static void Delete_employee(){}
     public static void Modify_employee(){}
 
@@ -184,5 +246,10 @@ public class Main
     public static void New_Goods(){}
     public static void Delete_Goods(){}
     public static void Modify_Goods(){}
+
+
+
+
+
 
 }
